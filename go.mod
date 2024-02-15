84a50fb94625f6a9bd2fe89e6ee7813c61b376a2
Matthew Halse <matthew.halse@hey.com>
2024-02-14 23:18:57 -0600

    Initial commit with custom router

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mdhalse/lenslocked
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:matthew.halse@hey.com\">matthew.halse@hey.com</a>")
+}
+
+type Router struct {}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+func main() {
+	var router Router
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", router)
+}
