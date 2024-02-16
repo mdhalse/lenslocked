7c97a4a9bcb8e4cc5c5beeb3833b9c25c623501d
Matthew Halse <matthew.halse@hey.com>
2024-02-16 16:08:17 -0600

    Add some middleware and try out URL params

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -30,12 +31,27 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	`)
 }
 
+func greeterHandler(w http.ResponseWriter, r *http.Request) {
+	nameParam := chi.URLParam(r, "name")
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write([]byte(fmt.Sprintf("<h1>Hello!<h2><p>Greetings to %s", nameParam)))
+}
+
 func main() {
 	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+	r.Get("/greeter/{name}", greeterHandler)
 	r.NotFound(http.NotFound)
+
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
 }
