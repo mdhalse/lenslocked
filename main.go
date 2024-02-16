7c52b7250cd6c10e2f2869da811a70662a7ffad7
Matthew Halse <matthew.halse@hey.com>
2024-02-16 15:27:57 -0600

    Add a FAQ page

--- main.go
+++ main.go
@@ -15,7 +15,20 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:matthew.halse@hey.com\">matthew.halse@hey.com</a>")
 }
 
-type Router struct {}
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `
+		<h1>FAQs</h1>
+		<p><strong>Q:</strong> Is there a free version?<p>
+		<p><strong>A:</strong> Yes!  We offer a free trial for 30 days on any paid plans.<p>
+		<p><strong>Q:</strong> What are your support hours?<p>
+		<p><strong>A:</strong> We have support staff answering emails 24/7, though response times may be a bit slower on the weekend.<p>
+		<p><strong>Q:</strong> How do I contact support?<p>
+		<p><strong>A:</strong> Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a><p>
+	`)
+}
+
+type Router struct{}
 
 func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
@@ -23,6 +36,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.NotFound(w, r)
 	}
