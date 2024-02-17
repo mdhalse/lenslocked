ea1c9820aec42498d0cdff9325eb5b1ec89e1175
Matthew Halse <matthew.halse@hey.com>
2024-02-17 15:23:54 -0600

    Templatize the contact page

--- main.go
+++ main.go
@@ -11,10 +11,9 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, fp string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tmplPath := filepath.Join("templates", "home.gohtml")
-	tmpl, err := template.ParseFiles(tmplPath)
+	tmpl, err := template.ParseFiles(fp)
 	if err != nil {
 		log.Printf("parsing template: %v", err)
 		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
@@ -29,9 +28,14 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tmplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tmplPath)
+}
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:matthew.halse@hey.com\">matthew.halse@hey.com</a>")
+	tmplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tmplPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
