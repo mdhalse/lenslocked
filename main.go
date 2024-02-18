57f54dc431de1be7ed5bfa02f38dfe10c99f2798
Matthew Halse <matthew.halse@hey.com>
2024-02-18 17:57:26 -0600

    Add a statichandler controller

--- main.go
+++ main.go
@@ -2,43 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/mdhalse/lenslocked/controllers"
 	"github.com/mdhalse/lenslocked/views"
 )
-
-func executeTemplate(w http.ResponseWriter, fp string) {
-	t, err := views.Parse(fp)
-	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
-		return
-	}
-	t.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "home.gohtml"))
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "contact.gohtml"))
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
-}
-
-func greeterHandler(w http.ResponseWriter, r *http.Request) {
-	nameParam := chi.URLParam(r, "name")
-
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "<h1>Hello!</h1><p>Greetings to %s</p>", nameParam)
-}
 
 func main() {
 	r := chi.NewRouter()
@@ -48,10 +19,23 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
-	r.Get("/greeter/{name}", greeterHandler)
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(tpl))
 	r.NotFound(http.NotFound)
 
 	fmt.Println("Starting the server on :3000...")
