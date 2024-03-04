a25829d268a70c9af4afff1e1b769f484a1c310d
Matthew Halse <matthew.halse@hey.com>
2024-03-03 22:05:41 -0600

    Experiment with the pgx package

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -41,4 +41,87 @@ func main() {
 	}
 
 	fmt.Println("Connected to db!")
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+			id SERIAL PRIMARY KEY,
+			name TEXT,
+			email TEXT UNIQUE NOT NULL
+		);
+
+		CREATE TABLE IF NOT EXISTS orders (
+			id SERIAL PRIMARY KEY,
+			user_id INT NOT NULL,
+			amount INT,
+			description TEXT
+		);
+	`)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Tables created!")
+
+	id := 3
+	row := db.QueryRow(`
+		SELECT name,
+		       email
+		  FROM users
+		 WHERE id = $1;
+	`, id)
+
+	var name, email string
+	err = row.Scan(&name, &email)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("User info: name=%s, email=%s\n", name, email)
+
+	//userID := 1
+	//for i := 1; i <= 5; i++ {
+	//	amount := i * 100
+	//	desc := fmt.Sprintf("Fake order #%d", i)
+	//	_, err := db.Exec(`
+	//		INSERT INTO orders (user_id, amount, description)
+	//		VALUES ($1, $2, $3)
+	//	`, userID, amount, desc)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//}
+	//fmt.Println("Created fake orders!")
+
+	type Order struct {
+		ID          int
+		UserID      int
+		Amount      int
+		Description string
+	}
+	var orders []Order
+	userID := 1
+	rows, err := db.Query(`
+		SELECT id, amount, description
+		  FROM orders
+		 WHERE user_id = $1
+	`, userID)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	
+	for rows.Next() {
+		var order Order
+		order.UserID = userID
+		err := rows.Scan(&order.ID, &order.Amount, &order.Description)
+		if err != nil {
+			panic(err)
+		}
+		orders = append(orders, order)
+	}
+	if rows.Err() != nil {
+		panic(rows.Err())
+	}
+
+	fmt.Println("Orders:", orders)
 }
