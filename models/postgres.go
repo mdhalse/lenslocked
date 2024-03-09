2995f7e829ffb990a0f686191f5b042a86f5b7e4
Matthew Halse <matthew.halse@hey.com>
2024-03-09 16:30:42 -0600

    Add infrastructure for connecting to postgres

---
+++ models/postgres.go
@@ -0,0 +1,43 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+// This function opens a connection to a Postgres database.
+// If you call this function, you must call db.Close() to
+// close the connection to the database.
+func Open(config PostgresConfig) (*sql.DB, error) {
+	db, err := sql.Open("pgx", config.String())
+	if err != nil {
+		return nil, fmt.Errorf("open: %w", err)
+	}
+	return db, nil
+}
+
+func DefaultPostgresConfig() PostgresConfig {
+	return PostgresConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "baloo",
+		Password: "junglebook",
+		Database: "lenslocked",
+		SSLMode:  "disable",
+	}
+}
+
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Host, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+}
